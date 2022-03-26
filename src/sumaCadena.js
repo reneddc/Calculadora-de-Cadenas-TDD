4f59e88701882e15069d122478888a8e787f767e
“Rene <rene.diaz@ucb.edu.bo>
2022-03-26 17:50:34 -0400

    Funcion 4-----Cadena mayor a 1000 igual a 0

--- src/sumaCadena.js
+++ src/sumaCadena.js
@@ -1,7 +1,6 @@
 class SumaCadena{
     sumaCadenas(cadena){
-        if(cadena.length == 0 || this.mayor1000(cadena))
-        {
+        if(cadena.length == 0 || this.mayor1000(cadena)){
             cadena = 0;
         }
         return parseInt(cadena);
